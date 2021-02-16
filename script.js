45ccefd1f00c88f2da30cef5ac091db4229acee7
phthaophan <phuong-thao1402@web.de>
2021-02-16 21:25:39 +0000

    stop at different positions, text disappears

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ import { VRButton } from './sources/VRButton.js';
 
 //FRAMERATE STATS 
 
-/*javascript: (function () { var script = document.createElement('script'); script.onload = function () { var stats = new Stats(); document.body.appendChild(stats.dom); requestAnimationFrame(function loop() { stats.update(); requestAnimationFrame(loop) }); }; script.src = '//mrdoob.github.io/stats.js/build/stats.min.js'; document.head.appendChild(script); })()*/
+javascript: (function () { var script = document.createElement('script'); script.onload = function () { var stats = new Stats(); document.body.appendChild(stats.dom); requestAnimationFrame(function loop() { stats.update(); requestAnimationFrame(loop) }); }; script.src = '//mrdoob.github.io/stats.js/build/stats.min.js'; document.head.appendChild(script); })()
 
 // 🌐 GLOBAL VARIABLES -------------------------- 
 
@@ -16,6 +16,7 @@ let phi = 0, theta = 0;
 let houses = [];
 let platforms = [];
 let mesh;
+let tweetColor = "white";
 
 let pChangeStart = 0.5;
 let pChangeEnd = 0.8;
@@ -28,12 +29,14 @@ const mouse = new THREE.Vector2();
 const target = new THREE.Vector2();
 const windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
 
+let scrollSpeed = 0.00002;
+let hadPaused = false;
 window.addEventListener('wheel', function (wheelEvent) {
   // wheelEvent.preventDefault();
-
   wheelEvent.stopPropagation();
-  userSpeed += wheelEvent.deltaY * 0.00002;
-})
+  userSpeed += wheelEvent.deltaY * scrollSpeed;
+  console.log(wheelEvent.deltaY);
+});
 
 window.addEventListener('mousemove', onMouseMove, false);
 
@@ -59,8 +62,8 @@ let near = 0.01;
 let far = 100;
 camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-//Camera Path
-//Vector values: (- = nachlinks/+ = nachrechts, - = nach unten/+ = nach oben, - = nach hinten/ + = nach vorne)
+// CAMERA PATH 
+// Vector values: (- = nachlinks/+ = nachrechts, - = nach unten/+ = nach oben, - = nach hinten/ + = nach vorne)
 
 const pathCurve = new THREE.CatmullRomCurve3([
   // Froschperspektive
@@ -97,7 +100,7 @@ const pathCurve = new THREE.CatmullRomCurve3([
   new THREE.Vector3(0, 19, 2),
   new THREE.Vector3(0, 20, 1),
   new THREE.Vector3(0, 22, 0.5),
-  new THREE.Vector3(0, 25, 0),
+  new THREE.Vector3(0, 23, 0),
 ]);
 
 const pathPoints = pathCurve.getPoints(50);
@@ -144,6 +147,11 @@ const hemiLight = new THREE.HemisphereLight(skyColor, groundColor, hemiIntensity
 hemiLight.position.set(0, 0, 0);
 scene.add(hemiLight);
 
+const ambiColor = 0x404040;
+const ambiIntensity = 1;
+const ambiLight = new THREE.AmbientLight(ambiColor, ambiIntensity);
+scene.add(ambiLight);
+
 const dirColor = 0xffffff;
 const dirIntensity = 1.75;
 const dirLight = new THREE.DirectionalLight(dirColor, dirIntensity);
@@ -163,33 +171,48 @@ dirLight.shadow.mapSize.height = 2048;
 dirLight.position.multiplyScalar(5); // Licht multiplizieren, um Schatten weicher zu machen 
 scene.add(dirLight);
 scene.add(dirLight.target);
-/*
-var pointLight = new THREE.PointLight(0xCE9178, 2.0, 600);
-pointLight.position.set = (4.5, 3, 3)
+
+let pointColor = 0xCE9178;
+let pointIntensity = 0.1;
+let pointDistance = 1;
+let pointDecay = 0;
+let pointHeight = 2;
+
+var pointLight = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight.position.set(0, pointHeight, 0);
 scene.add(pointLight);
 
-var pointLight2 = new THREE.PointLight(0xCE9178, 2.0, 600);
-pointLight2.position.set = (4.5, 3, -3)
+var pointLight2 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight2.position.set(6, pointHeight, 4);
 scene.add(pointLight2);
 
-var pointLight3 = new THREE.PointLight(0xCE9178, 2.0, 600);
-pointLight3.position.set = (-4.5, 3, -3)
+var pointLight3 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight3.position.set(6, pointHeight, -4);
 scene.add(pointLight3);
 
-var pointLight4 = new THREE.PointLight(0xCE9178, 2.0, 600);
-pointLight4.position.set = (-4.5, 3, 3)
-scene.add(pointLight4); */
+var pointLight4 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight4.position.set(-6, pointHeight, -4);
+scene.add(pointLight4);
 
-const color = 0xFFFFFF;
-const intensity = 1;
-const light = new THREE.PointLight(color, intensity);
-light.position.set(0, 2, 0);
-scene.add(light);
+var pointLight5 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight5.position.set(-6, pointHeight, 4);
+scene.add(pointLight5);
 
-const ambiColor = 0x404040;
-const ambiIntensity = 1;
-const ambiLight = new THREE.AmbientLight(ambiColor, ambiIntensity);
-scene.add(ambiLight);
+var pointLight6 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight6.position.set(0, pointHeight, -4);
+scene.add(pointLight6);
+
+var pointLight7 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight7.position.set(0, pointHeight, 4);
+scene.add(pointLight7);
+
+var pointLight8 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight8.position.set(-6, pointHeight, 0);
+scene.add(pointLight8);
+
+var pointLight9 = new THREE.PointLight(pointColor, pointIntensity, pointDistance, pointDecay);
+pointLight9.position.set(6, pointHeight, 0);
+scene.add(pointLight9);
 
 // 🎛 RENDER SETTINGS -------------------------- 
 
@@ -253,9 +276,7 @@ class House {
     this.tweetString = _tweetString;
 
     this.dynamicTexture = new THREEx.DynamicTexture(400, 400 * this.height)
-
     this.dynamicTexture.clear('rgb(29,41,81)')
-
 
     // 🏠 GEOMETRY OF THE HOUSE 
 
@@ -270,9 +291,22 @@ class House {
     // die Dächer der Häuser sollen zu 10% weiß und 90% schwarz sein und das durch eine zufällige Anordnung
     let colorProbability = Math.random();
 
-    if (colorProbability < 0.5) {
+    if (colorProbability < 0.2) {
       this.roofColor = "rgb(255,255,255)";
     }
+
+    this.checkText = true;
+
+    this.dynamicTexture.drawTextCooked({
+      background: "black", // der Hintergrund muss schwarz sein, damit die emissiveMap (als Maske) funktioniert
+      text: this.tweetString,
+      lineHeight: 0.1 / this.height,
+      emissive: 1,
+      blending: THREE.AdditiveBlending,
+      fillStyle: tweetColor,
+      font: "24px Helvetica",
+      marginTop: ((this.height - this.fixedBoxSizeY + 1) / this.height) // da fixedBoxSize noch zu hoch ist.
+    })
 
     this.material = [
       new THREE.MeshPhongMaterial({
@@ -339,28 +373,7 @@ class House {
     this.mesh.position.z = this.zPos + this.depth / 2;
   }
 
-
-
   animate() {
-
-    // Draw Texture with Color Change after Changing perspective
-
-    this.tweetColor = "white";
-
-    if (userPosition > 0.5) {
-      this.tweetColor = "black";
-    }
-
-    this.dynamicTexture.drawTextCooked({
-      background: "black", // der Hintergrund muss schwarz sein, damit die emissiveMap (als Maske) funktioniert
-      text: this.tweetString,
-      lineHeight: 0.1 / this.height,
-      emissive: 1,
-      blending: THREE.AdditiveBlending,
-      fillStyle: this.tweetColor,
-      font: "24px Helvetica",
-      marginTop: ((this.height - this.fixedBoxSizeY + 1) / this.height) // da fixedBoxSize noch zu hoch ist.
-    })
 
     // 🏢 GROWING HOUSES 
 
@@ -379,6 +392,7 @@ class House {
     //Achtung: growingSpeed und MaxScale sind abhängig von einander. Wenn man oben den growingSpeed vergößert muss hier der Scale (diese * 200) verkleinert werden. Zb. wird der Speed verdoppelt muss der Scale hier halbiert werden um dieselbe maxScale zu erreichen wie zuvor. 1 ist eine notwendige Konstante.
 
     // die Häuser sollen nur beim Perspektivenwechseln wachsen und bis sie maxScale erreicht haben
+
     if (userPosition > pChangeStart && userPosition < pChangeEnd && this.mesh.scale.y < maxScale) {
       this.mesh.scale.y += Math.random() * growingSpeed;
     } else if (userPosition >= 0 && userPosition < 0.4) {
@@ -387,10 +401,17 @@ class House {
       this.mesh.scale.y = this.mesh.scale.y;
     }
 
-    if (this.mesh.scale.y >= 0) {
-      console.log(this.mesh.scale.y);
-      this.tweetColor = "black";
+    if (userPosition > 0.5 && this.checkText) {
+      this.dynamicTexture.drawTextCooked({
+        background: "black",
+        text: '',
+        emissive: 1,
+        blending: THREE.AdditiveBlending,
+        fillStyle: this.tweetColor,
+      });
+      this.checkText = false;
     }
+
   }
 }
 
@@ -524,9 +545,26 @@ function animate(renderer, scene, camera) {
     camera.lookAt(0, 7, 0);
   }
 
+  // STOP AT PERSPECTIVE CHANGE 
+
+  if (userPosition > 0.46 && userPosition < 0.47 && !hadPaused) {
+    scrollSpeed = 0;
+    setTimeout(function () { scrollSpeed = 0.00002; }, 1000);
+  } else 
+
+  if (userPosition > 0.69 && userPosition < 0.7 && !hadPaused) {
+    scrollSpeed = 0;
+    setTimeout(function () { scrollSpeed = 0.00002; hadPaused = true; }, 1000);
+  }
+
   // BUTTONS 
+
   document.getElementById("replay").onclick = function () {
     window.location.reload();
+  };
+
+  document.getElementById("popup").onclick = function () {
+    getElementById("popup").style.display = absolute;
   };
 
   document.getElementById("start").onclick = function () {
@@ -542,76 +580,82 @@ function animate(renderer, scene, camera) {
     document.getElementById("scrollbox1").style.display = "flex";
   } else {
     document.getElementById("scrollbox1").style.opacity = 0;
-    setTimeout(function () {
-      document.getElementById("scrollbox1").style.display = "none";
-    }, transitionTime);
+    setTimeout(function () { (document.getElementById("scrollbox1").style.display = "none") }, transitionTime);
   }
 
   if (userPosition > 0.02 && userPosition < 0.04) {
     document.getElementById("scrollbox2").style.opacity = 1;
-    setTimeout(function () {
-      document.getElementById("scrollbox2").style.display = "flex";
-    }, 1000);
+    setTimeout(document.getElementById("scrollbox2").style.display = "flex", 1000);
   } else if (userPosition > 0.04 && userPosition < 0.06) {
     document.getElementById("scrollbox2").style.opacity = 0;
-    setTimeout(function () {
-      document.getElementById("scrollbox2").style.display = "none";
-    }, transitionTime);
+    setTimeout(function () { (document.getElementById("scrollbox2").style.display = "none") }, transitionTime);
   } else if (userPosition < 0.02) {
     document.getElementById("scrollbox2").style.opacity = 0;
     document.getElementById("scrollbox2").style.display = "none";
   }
 
-  if (userPosition > 0.15 && userPosition < 0.19) {
+  if (userPosition > 0.18 && userPosition < 0.25) {
     document.getElementById("scrollbox3").style.opacity = 1;
     document.getElementById("scrollbox3").style.display = "flex";
-  } else if (userPosition > 0.19 && userPosition < 0.21) {
+  } else if (userPosition > 0.17 && userPosition < 0.26) {
     document.getElementById("scrollbox3").style.opacity = 0;
-    setTimeout(function () {
-      document.getElementById("scrollbox3").style.display = "none";
-    }, transitionTime);
-  } else if (userPosition < 0.15) {
+    setTimeout(function () { (document.getElementById("scrollbox3").style.display = "none") }, transitionTime);
+  } else {
     document.getElementById("scrollbox3").style.opacity = 0;
     document.getElementById("scrollbox3").style.display = "none";
   }
 
-  if (userPosition > 0.35 && userPosition < 0.4) {
+  if (userPosition > 0.30 && userPosition < 0.38) {
     document.getElementById("scrollbox4").style.opacity = 1;
     document.getElementById("scrollbox4").style.display = "flex";
-  } else if (userPosition > 0.4 && userPosition < 0.42) {
+  } else if (userPosition > 0.29 && userPosition < 0.39) {
     document.getElementById("scrollbox4").style.opacity = 0;
-    setTimeout(function () {
-      document.getElementById("scrollbox4").style.display = "none";
-    }, transitionTime);
-  } else if (userPosition < 0.35) {
+    setTimeout(function () { (document.getElementById("scrollbox4").style.display = "none") }, transitionTime);
+  } else {
     document.getElementById("scrollbox4").style.opacity = 0;
     document.getElementById("scrollbox4").style.display = "none";
   }
 
-  //Scrollbox 5
-  //pChangeStart = 0.5
-  //pChangeStart = 0.8 eigentlich wollte ich für das an und ein schalten der opacity
-  //noch eine Rechnung machen mit pChangeStart - 0.3 oder so xDDDD aber das wird too much oder?
+  // SCROLLBOX 5 - Changing perspectives.
 
-  if (userPosition > pChangeStart && userPosition < pChangeEnd) {
+  if (userPosition > 0.46 && userPosition <= 0.56) {
     document.getElementById("scrollbox5").style.display = "flex";
   } else {
     document.getElementById("scrollbox5").style.display = "none";
   }
 
-  if (userPosition > 0.53 && userPosition < 0.78) {
+  if (userPosition > 0.47 && userPosition <= 0.55) {
     document.getElementById("scrollbox5").style.opacity = 1;
   } else {
     document.getElementById("scrollbox5").style.opacity = 0;
   }
 
-  //Scrollbox 6
+  // SCROLLBOX 6 - We are almost there.
 
-  if (userPosition > 0.95 && userPosition <= 1) {
-    document.getElementById("scrollbox6").style.opacity = 1;
+  if (userPosition > 0.68 && userPosition <= 0.78) {
     document.getElementById("scrollbox6").style.display = "flex";
   } else {
     document.getElementById("scrollbox6").style.display = "none";
+  }
+
+  if (userPosition > 0.69 && userPosition <= 0.77) {
+    document.getElementById("scrollbox6").style.opacity = 1;
+  } else {
+    document.getElementById("scrollbox6").style.opacity = 0;
+  }
+
+  // SCROLLBOX 7 - The whole picture. 
+
+  if (userPosition > 0.90 && userPosition <= 0.99) {
+    document.getElementById("scrollbox7").style.display = "flex";
+  } else {
+    document.getElementById("scrollbox7").style.display = "none";
+  }
+
+  if (userPosition > 0.91 && userPosition <= 0.98) {
+    document.getElementById("scrollbox7").style.opacity = 1;
+  } else {
+    document.getElementById("scrollbox7").style.opacity = 0;
   }
 
   // 👀 CAMERA ROTATION ON MOUSE MOVEMENT 
@@ -633,14 +677,13 @@ function animate(renderer, scene, camera) {
     camera.rotation.y = 0;
   }
 
-  // Makes houses grow
+  // MAKES HOUSES GROW 
 
   for (let i = 0; i < houses.length; i++) {
     houses[i].animate();
   }
 
   renderer.render(scene, camera);
-
   requestAnimationFrame(function () {
     animate(renderer, scene, camera);
   });
